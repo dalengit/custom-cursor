85181d79525ae5e32791b6fefb845ac796ace494
Dalen Chan <dalen.chan@hotmail.com>
2021-07-04 19:07:06 +0100

    JS click list add

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ window.addEventListener('mousemove', (e) => {
     cursor.style.left = e.pageX + 'px';
     cursor.style.top = e.pageY + 'px';
 })
+
+window.addEventListener( 'click', () => {
+    if(cursor.classList.contains('click')) {
+        cursor.classList.remove('click');
+        void cursor.offsetWidth;
+        cursor.classList.add('click');
+    } else {
+        cursor.classList.add('click');
+    }
+})
